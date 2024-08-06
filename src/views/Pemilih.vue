9971d06c7f3db44327d6b7dde5c9d6ef79b277b0
Noval Smith <novalsmith69@gmail.com>
2024-08-06 07:34:22 +0700

    Add detail status activating

--- src/views/Pemilih.vue
+++ src/views/Pemilih.vue
@@ -13,7 +13,7 @@
                                 hide-details></v-text-field>
                         </v-toolbar>
                     </v-card-title>
-                    <v-data-table :headers="headers" :items="filteredVolunteers" multi-sort :headerProps="headerprops"
+                    <v-data-table :headers="headers" :items="filteredVolunteers" multi-sort :header-props="headerprops"
                         class="elevation-1 table-style" :loading="isLoading"
                         :loading-text="isLoading ? 'Loading... Please wait' : ''" @click:row="rowClick" :footer-props="{
                                 showFirstLastPage: true,
@@ -33,20 +33,93 @@
                                 <v-icon class="ml-2" v-if="item.statusName == 'Sudah'">mdi-check-circle-outline</v-icon>
                             </v-chip>
                         </template>
-
                         <template v-slot:item.attachmentName="{ item }">
                             <v-icon color="green" v-if="item.attachmentName != ''">mdi-check-circle-outline</v-icon>
                             <v-icon color="grey" v-else>mdi-close-circle-outline</v-icon>
-
-
-
-
                         </template>
-
                     </v-data-table>
                 </v-card>
             </v-col>
         </v-row>
+
+        <!-- Dialog untuk menampilkan detail data -->
+        <v-dialog v-model="dialog" max-width="400px">
+            <v-card>
+                <v-card-title>
+                    Detail Data
+                    <v-spacer></v-spacer>
+                    <v-btn icon @click="dialog = false">
+                        <v-icon>mdi-close-circle-outline</v-icon>
+                    </v-btn>
+                </v-card-title>
+                <v-card-subtitle>
+                    <v-row>
+                        <!-- Kolom untuk Data Diri -->
+
+                        <v-col cols="12">
+                            <v-alert color="orange lighten-2">
+                                <strong>[Perhatian]</strong>
+                                Silahkan Aktifkan Jika data pemilih sudah benar,
+                                dan jangan diaktifkan jika data belum benar.
+                            </v-alert>
+                        </v-col>
+                        <v-col cols="12">
+                            <small class="mr-1 mb-1">KTP/SIM</small>
+                            <v-icon color="green"
+                                v-if="selectedItem.attachmentName != ''">mdi-check-circle-outline</v-icon>
+                            <v-icon color="grey" v-else>mdi-close-circle-outline</v-icon>
+                            <v-img max-width="500" :src="imageLink"></v-img>
+                        </v-col>
+                        <v-col cols="12">
+                            <h3>1. Data Diri</h3>
+                            <p><strong>Nama:</strong> {{ selectedItem.name }}</p>
+                            <p><strong>Email:</strong> {{ selectedItem.email }}</p>
+                            <p><strong>Tlp/WhatsApp:</strong> {{ selectedItem.phone }}</p>
+                            <p><strong>Alamat:</strong> {{ selectedItem.address }}</p>
+                            <p><strong>Jenis Kelamin:</strong> {{ selectedItem.gender }}</p>
+                            <p><strong>Tanggal Lahir:</strong> {{ selectedItem.birthDate }}</p>
+                            <p v-if="selectedItem.imageLink != ''">
+                                <strong>Gambar KTP/SIM:</strong>
+                                <img :src="selectedItem.imageLink" width="200">
+                            </p>
+                        </v-col>
+
+                        <!-- Kolom untuk Alamat Tinggal -->
+                        <v-col cols="12">
+                            <h3>2. Alamat Tinggal / Daerah Pemilihan</h3>
+                            <p><strong>Kabupaten/Kota:</strong> {{ selectedItem.regency }}</p>
+                            <p><strong>Kecamatan:</strong> {{ selectedItem.district }}</p>
+                            <p><strong>Kelurahan:</strong> {{ selectedItem.ward }}</p>
+                            <p><strong>Desa:</strong> {{ selectedItem.village }}</p>
+                            <p><strong>RT:</strong> {{ selectedItem.rt }}</p>
+                            <p><strong>RW:</strong> {{ selectedItem.rw }}</p>
+                        </v-col>
+
+                        <!-- Kolom untuk Daerah Pemenangan Relawan -->
+                        <v-col cols="12" v-if="selectedItem.role == 'relawan'">
+                            <h3>3. Daerah Pemenangan Relawan</h3>
+                            <p><strong>Nama Relawan:</strong> {{ selectedItem.volunteerName }}</p>
+                            <p><strong>Kabupaten/Kota Pemenangan:</strong> {{ selectedItem.volunteersRegencyName }}</p>
+                            <p><strong>Kecamatan Pemenangan:</strong> {{ selectedItem.volunteersDistrictName }}</p>
+                        </v-col>
+                    </v-row>
+                </v-card-subtitle>
+                <div class="fixed-card-actions">
+
+                    <v-card-actions>
+
+                        <v-btn v-if="selectedItem.status == 'inactive'" color="green" dark
+                            @click="toggleStatus('Aktif')">Aktifkan <v-icon>mdi-check-circle-outline</v-icon></v-btn>
+                        <v-btn v-if="selectedItem.status == 'active'" color="red" dark
+                            @click="toggleStatus('Tidak Aktif')">Tidak Aktif</v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn text @click="dialog = false">Close </v-btn>
+                    </v-card-actions>
+                </div>
+
+
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 
@@ -56,18 +129,6 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
     name: "Hero",
-    computed: {
-        ...mapGetters(['isLoggedIn', 'username', 'userData', 'token', 'regencies', 'isOverlayLoading', 'userLogin']),
-        filteredVolunteers() {
-            if (!this.search) return this.volunteers;
-            return this.volunteers.filter(volunteer => {
-                const searchLower = this.search.toLowerCase();
-                return Object.keys(volunteer).some(key =>
-                    String(volunteer[key]).toLowerCase().includes(searchLower)
-                );
-            });
-        },
-    },
     data() {
         return {
             color: "grey darken-2",
@@ -98,7 +159,22 @@ export default {
                 { text: 'KTP/SIM', value: 'attachmentName' },
                 { text: 'Verifikasi', value: 'statusName' },
             ],
+            dialog: false, // Untuk mengontrol tampilan dialog
+            selectedItem: {}, // Data baris yang dipilih
+            imageLink: '' // Link untuk gambar
         }
+    },
+    computed: {
+        ...mapGetters(['isLoggedIn', 'username', 'userData', 'token', 'regencies', 'isOverlayLoading', 'userLogin']),
+        filteredVolunteers() {
+            if (!this.search) return this.volunteers;
+            return this.volunteers.filter(volunteer => {
+                const searchLower = this.search.toLowerCase();
+                return Object.keys(volunteer).some(key =>
+                    String(volunteer[key]).toLowerCase().includes(searchLower)
+                );
+            });
+        },
     },
     methods: {
         ...mapActions(['showOverlayLoading', 'hideOverlayLoading']),
@@ -111,8 +187,12 @@ export default {
                 this.isLoading = false;
             }
         },
-        rowClick(row) {
-            console.log(row);
+        async rowClick(row) {
+            const userDetailsParam = { "volunteerID": row.volunteerID };
+            var listData = await axios.post(process.env.VUE_APP_SERVICE_URL + "search/userByID", userDetailsParam);
+            this.selectedItem = listData.data.data[0];
+            this.imageLink = process.env.VUE_APP_SERVICE_URL + "core/public/attachment/" + this.selectedItem.attachmentName; // Set link gambar
+            this.dialog = true; // Tampilkan dialog
         },
         async getVolunteers() {
             this.showOverlayLoading();
@@ -123,7 +203,6 @@ export default {
                 this.volunteers = userDetailsRes.data.data;
                 if (this.userLogin.role == 'relawan') {
                     this.volunteers = userDetailsRes.data.data.filter(value => value.parent == this.username.toUpperCase());
-
                 }
                 console.log(this.userLogin);
             } catch (error) {
@@ -132,61 +211,37 @@ export default {
                 this.hideOverlayLoading();
             }
         },
+        async toggleStatus(status) {
+            try {
+                const updatedItem = { ...this.selectedItem, statusName: status };
+                // Ganti URL dengan endpoint yang sesuai untuk memperbarui status
+                await axios.post(process.env.VUE_APP_SERVICE_URL + "update/status", updatedItem);
+                // Perbarui data lokal
+                this.selectedItem.statusName = status;
+                this.dialog = false; // Tutup dialog setelah memperbarui status
+            } catch (error) {
+                console.log(error);
+            }
+        }
     },
-    async created() {
-        await this.getVolunteers(); // Fetch volunteer data on created
-    },
-    async mounted() {
-        // await this.loadRegencies();
+    mounted() {
+        this.getData();
+        this.getVolunteers();
     }
-}
+};
 </script>
+
 <style scoped>
-.intro-text {
-    font-size: 40px;
-    font-weight: 600;
-    font-family: 'Roboto';
-    line-height: 40pt;
-}
-
-p.description {
-    font-size: 19px;
-    margin: 32px 0px;
-}
-
-.errorColor {
-    background-color: #FFCDD2 !important;
-}
-
-#table>.v-data-footer .v-icon {
-    color: black !important;
-}
-
-.table-style>>>tbody tr:hover {
-    cursor: pointer;
-    background: #0097A7 !important;
-    color: white;
-}
-
-@media (max-width: 600px) {
-    .intro-text {
-        font-size: 24px;
-    }
-
-    p.description {
-        font-size: 16px;
-    }
-
-    .text-h5 {
-        font-size: 18px;
-    }
-
-    .text-h2 {
-        font-size: 24px;
-    }
-}
-
 .table-style {
-    margin-bottom: 16px;
+    font-size: 0.875rem;
+    font-weight: 400;
+    border-radius: 4px;
+}
+
+.fixed-card-actions {
+    position: sticky;
+    bottom: 0;
+    background-color: white;
+    z-index: 1;
 }
 </style>
